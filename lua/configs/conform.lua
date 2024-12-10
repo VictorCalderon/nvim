415dd0ac2ea40f47443f0f3b8a238d5a73fbb9c5
Victor Calderon <victorvirgilioc@gmail.com>
2024-12-10 12:52:46 -0500

    feat: add missing lsps and theming

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,20 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+    go = { "goimports", "gofmt" },
+    javascript = { { "prettierd", "prettier" } },
+    python = { "ruff", "isort" },
+    rust = { "rustfmt", lsp_format = "fallback" },
+    terraform = { "terraform_fmt" }
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,3 +1,11 @@
+ -- put this in your main init.lua file ( before lazy setup )
+ vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46_cache/"
+
+ -- put this after lazy setup
+ dofile(vim.g.base46_cache .. "defaults")
+ dofile(vim.g.base46_cache .. "statusline")
+
+
 return {
   {
     "stevearc/conform.nvim",
@@ -13,13 +21,44 @@ return {
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css", "terraform"
+  		},
+  	},
+  },
+
+  -- Git
+  {
+    "tpope/vim-fugitive",
+    lazy = false,
+  },
+
+  -- Best plugin ever
+  {
+    "eandrju/cellular-automaton.nvim",
+    lazy = false,
+  },
+
+ "nvim-lua/plenary.nvim",
+
+ {
+   "nvchad/ui",
+    config = function()
+      require "nvchad"
+    end
+ },
+
+ {
+    "nvchad/base46",
+    lazy = true,
+    build = function()
+      require("base46").load_all_highlights()
+    end,
+ },
+
+ "nvzone/volt", -- optional, needed for theme switcher
 }
